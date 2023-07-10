82f6a77a83caf3485fb89af5956b7cf25572f71d
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-07-10 15:22:57 -0300

    funcion1
    se llama una funcion y se le pasan dos argumentos, dichos argumentos seran datos locales que actuarán dentro del ámbito de la función, la funcion acepta estos dos (ahora) parámteros y produce una salida (datos de comunicación) hacia el exterior, en este caso se trara de un string. Se retorna como una concatenacion ("texto"+variable) o como una cadena templada con comillas inversas (` `)

---
+++ index.js
@@ -0,0 +1,28 @@
+// FUNCIONES
+
+//Definición de una función
+// cuando se define la función, también se definen
+// los parámetros. EJ.: Son dos parametros
+// nombre y apellido. 
+//los parámetros son los datos de los argumentos
+//que se reciben.
+function saludar(nombre, apellido) {
+    //datos locales nombre y apellido
+    
+    // instrucción (return)
+    // return 'Hola ' + nombre + ' ' + apellido + '.';
+
+    //datos de comunicacion es el string que
+    //sale de la funcion,
+    //ej: "Hola Laura Garcia"
+    
+    //con las comillas alrevés o ``
+    // se denomina cadena templada o string template
+    return `Hola ${ nombre } ${ apellido }.`;
+}
+//cuando se llama o se invoca a una funcion
+//luego del nombre se pone los paréntesis
+//en caso que sea necesario se pasan los 
+//ARGUMENTOS a la función saludar.
+//los argumentos son los datos que se envian
+console.log(saludar("Laura", "García"));
