0615a044168158f08c028c7939e3d2fdbd87d0c6
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-09-18 22:01:30 -0300

    keys con las li (listas), map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   // crear un arreglo con numeros del 0 al 9
   const lista = [0, 1, 2];
+   // mapear la lista y lo guardo en la constante
+  const listaMapeada = lista.map(item => <li key={item}>{item}</li>);
 
   const colores = [{
     id: 'rojo',
@@ -10,13 +12,30 @@ function App() {
   }, {
     id: 'azul',
     color: "blue"
-    }, {
+  }, {
     id: 'verde',
     color: "green"
-  }]
+  }];
 
-  // mapear la lista y lo guardo en la constante
-  const listaMapeada = lista.map(item => <li>{item}</li>);
+  const nombres = [
+    {
+      llama: 'Pedro'
+    },
+    {
+      llama: 'Juan'
+    },
+    {
+      llama: 'Perez'
+    },
+    {
+      llama: 'Hernandez'
+    },
+  ];
+
+  // agrego el index denominado indice
+  const nombresPersonas = nombres.map((persona, indice) =>
+    <li key={indice}>name: {persona.llama} - indice: {indice}</li>);
+  
   return (
     <>
       <h1>Renderizar listas en JSX</h1>
@@ -36,9 +55,17 @@ function App() {
       <ul>{listaMapeada}</ul>
 
       <h2>Iteracion con un objeto colores</h2>
-      <ol>{colores.map((color) => <li style={{color: color.color}}>El color es: {color.id}</li>)}</ol>
+      {/* del objeto colores esta tiene dos propiedades el id (nombre del color en castellano) que 
+      se muestra en la lista y que se pone como  llave o key; por otro lado la propiedad color
+      (nombre del color como lo acepta css para mostrar el estilo) */}
+      <ol>{colores.map((color) => <li style={{ color: color.color }} key={color.id}>El color es: {color.id}</li>)}</ol>
+    
+      <h2>Nombres de Personas</h2>
+      <ul>{nombresPersonas}</ul>
     </>
   );
 }
 
 export default App;
+
+// 19:46
