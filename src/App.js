19d4a2397e3e432c85b5480cc9314fa3b83de077
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-11-03 20:29:15 -0300

    crear una lista de compras metodo 1
    Creacion de una lista de compras la cual paso desde un componente padre la lista mapeada (item.id) con la denominacion arbitraria de atributos (itemID.....). En el componente hijo muestro cada registro usando props y mostrando cada datos obtenido (props.itemID)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CicloVidaComponente from './components/cicloVidaComponente';
+import ListaCompras from './components/listaCompras';
 
 function App() {
   return (
     <>
       <CicloVidaComponente />
+      <ListaCompras />
     </>
   );
 }
---
+++ src/components/itemListaProducto.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// recibo desde el componente listaCompras los datos
+// y aquí los denomino props
+const ItemListaProducto = (props) => {
+    return (
+        <>
+            {/* muestro por cada registro sus datos
+            con props (el registro que estoy pasando
+            y el atributo tal cual lo asigné y pase en 
+            listaCompas) */}
+            <li>{props.itemID} - {props.itemNombre} - - ${props.itemPrecio}.- tiene {props.itemStock} unidades en stock.</li>
+        </>
+    );
+}
+
+export default ItemListaProducto;
