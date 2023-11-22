5a7e6c9cbdaef2f9542ec26f5b28fa095c9c5d00
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-11-22 00:25:37 -0300

    Usando Link to (reemplazo del href)
    el href hace que la página SE RECARGUE Y PEGUE SALTOS!!! mientras que el Link to hace que la página NO SE RECARGUE Y NO PEGUE SALTOS!!!

--- src/components/react-router/NavBar.js
+++ src/components/react-router/NavBar.js
@@ -1,19 +1,29 @@
 // crear un componente nav
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav>
             <ul>
                 {/* href es el path de las rutas (Route) */}
-                <li><a href='/'>Inicio</a></li>
+                {/* el href hace que la página SE RECARGUE Y PEGUE SALTOS!!! */}
+                {/* <li><a href='/'>Inicio</a></li>
                 <li><a href='/productos'>Productos</a></li>
                 <li><a href='/sobre-nosotros'>Sobre Nosotros</a></li>
-                <li><a href='/contacto'>Contacto</a></li>
+                <li><a href='/contacto'>Contacto</a></li> */}
+                
+                {/* to={'ruta'} */}
+                {/* el Link to hace que la página NO SE RECARGUE Y NO PEGUE SALTOS!!! */}
+                <li><Link to={'/'}>Inicio</Link></li>
+                <li><Link to={'/productos'}>Productos</Link></li>
+                <li><Link to={'/sobre-nosotros'}>Sobre Nosotros</Link></li>
+                <li><Link to={'/contacto'}>Contacto</Link></li>
             </ul>
         </nav>
     );
 }
 
 export default NavBar;
+// 33:41
