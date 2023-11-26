321e83c630ca18aa45d33f16a42d73b9548bb853
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-11-26 16:41:47 -0300

    agregar un boton mostrar contador, suma en dos, resta en dos, boton reset, manejo del action.payload

--- src/components/react-router/inicio.js
+++ src/components/react-router/inicio.js
@@ -11,6 +11,7 @@ import { TYPES } from './types';
 // contiene el estado inicial
 const initialState = {
     contador: 0,
+    mostrarContador: false,
     // ......
     // .......
      // otros atributos que se inicializan
@@ -35,13 +36,31 @@ const contadorReducer = (state, action) => {
             // console.log(state.contador);
             // retornar contador como state.contador+1 como objeto
             // y este objeto se imprime por pantalla ( <h3>{state.contador}</h3> )
-            return { contador: state.contador + 1 };
-        case TYPES.DECREMENTO:
-            return { contador: state.contador - 1 };
+            // return { contador: state.contador + 1 }; modificarlo porque desaparece
+            return { ...state, contador: state.contador + action.payload };
+        case TYPES.DECREMENTO:            
+        // return { contador: state.contador - 1 }; modificarlo porque desaparece
+            return { ...state, contador: state.contador - action.payload };
+        case TYPES.MOSTRAR_CONTADOR:
+            // aqui debo dejar el resto del estado como está (state.contador) y modificar
+            // el atributo mostrar contador, no puedo modificar directamente porque borro
+            // el resto de los atributos y deberé modificar Aumento y Decremento
+            // ...state, algo (dejo como esta el resto (lo que no se modifica) 
+            // y le agrego o modifico algo); si no existe se agrega de lo contrario se modifica.
+            // si hago clic en aumentar o disminuir se oculta el contador porque edito 
+            // solo el contador y no el mostrar.... debo modificarlo....
+            return { ...state, mostrarContador: !state.mostrarContador };
+        case TYPES.AUMENTO2:
+            return { ...state, contador: state.contador + action.payload };
+        case TYPES.DECREMENTO2:
+            return { ...state, contador: state.contador - action.payload };
+        case TYPES.RESETEAR:
+            return { ...state, contador: 0 };
         default:
             return state;
     }
 }
+
 
 
 
@@ -59,7 +78,7 @@ const Inicio = () => {
     // const aumentarHandler = () => dispach({ type: 'INCREMENTAR' })
 
     // como me puedo equivocar con los tipos llamo a TYPES
-    const aumentarHandler = () => dispach({ type: TYPES.AUMENTO });
+    const aumentarHandler = () => dispach({ type: TYPES.AUMENTO, payload: 1 });
 
     // El dispach llama a la funcion reductora (contadorReducer), y el objeto type ingresa
     // por action ( const contadorReducer = (state, action) => .... )
@@ -67,8 +86,14 @@ const Inicio = () => {
     // const disminuirHandler = () => dispach({ type: 'DECREMENTAR' })
 
     // como me puedo equivocar con los tipos llamo a TYPES
-    const disminuirHandler = () => dispach({ type: TYPES.DECREMENTO });
+    const disminuirHandler = () => dispach({ type: TYPES.DECREMENTO, payload: 1 });
 
+    const aumentarHandler2 = () => dispach({type: TYPES.AUMENTO, payload: 2});
+
+    const disminuirHandler2 = () => dispach({ type: TYPES.DECREMENTO, payload: 2 });
+    
+    const resetearHandler = () => dispach({ type: TYPES.RESETEAR });
+    
         
     return (
         <div>
@@ -77,12 +102,20 @@ const Inicio = () => {
             <p>Bienvenido a nuestra página</p>
             <p>Componente de inicio</p>
             <h2>Un contador con useReducer</h2>
-            <div style={{textAlign:'center'}}>
+            <h4>Contador</h4>
+            {/* el boton hay que mostrarlo siempre */}
+            {/* cuando presiono llamo al dispach que llama a types para no errar en el string
+            luego se debe agregar el caso */}
+            <button onClick={()=>dispach({type: TYPES.MOSTRAR_CONTADOR})}>Mostrar contador</button>
+            {state.mostrarContador && <div style={{ textAlign: 'center' }}>
                 <h3>{state.contador}</h3>
                 {/* onClick es un evento */}
+                <button onClick={disminuirHandler2}>Disminuir en 2</button>
                 <button onClick={disminuirHandler}>Disminuir</button>
                 <button onClick={aumentarHandler}>Aumentar</button>
-            </div>
+                <button onClick={aumentarHandler2}>Aumentar en 2</button>
+                <button onClick={resetearHandler}>RESET</button>
+            </div>}
         </div>
     );
 }
